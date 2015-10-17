f7db87d771291b7e54205fa1dd48bcef2eba493d
David Goldberg <drgold1988@gmail.com>
2015-10-16 18:02:22 -0700

    [Feature] creates Footer component

---
+++ src/footer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Footer = React.createClass({
+  render: function() {
+    return (
+      <div className='footer'>
+      Footer
+      </div>
+    );
+  }
+});
+
+module.exports = Footer;
